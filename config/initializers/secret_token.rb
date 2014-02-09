eed63c3e23ffa1abc339eb94e6ea6a742e36fd59
Attila Vago <attilavago85@gmail.com>
2014-02-09 14:11:02 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StiriCopii::Application.config.secret_token = '1fa6f6f142fb9bfc73afcecd3c07c1ef8161b84b65bcea2d91915f4245b09f82fe815b0dbdeb1dd863194b57b119149a0319c9b72b4275c7c3aff779368e54e4'
